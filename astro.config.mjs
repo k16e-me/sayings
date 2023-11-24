b0daf9915596324e108784dab209d0d9ec349e94
kb <kb@k16e.co>
2023-11-24 17:33:22 +0100

    ...

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,18 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import storyblok from '@storyblok/astro'
+import tailwind from '@astrojs/tailwind'
+import { loadEnv } from 'vite'
 
-import tailwind from "@astrojs/tailwind";
+const env = loadEnv('', process.cwd(), 'STORYBLOK')
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
-});
+    integrations: [
+        tailwind(),
+        storyblok({
+            accessToken: env.STORYBLOK_TOKEN,
+            components: {
+                // Add your components here
+            }
+        })
+    ]
+})

6c0795900b9be7526ee8dbf6a8a0c0a38f7d894c
kb <kb@k16e.co>
2023-11-24 23:40:24 +0100

    switched sans to dm

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,23 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
         extend: {
             fontFamily: {
+                sans: ['DM Sans Variable', ...defaultTheme.fontFamily.sans],
                 display: 'Limelight, sans- serif'
             },
             colors: {
                 brand: {
-                    '50': '#f7f7ef',
-                    '100': '#efeedf',
-                    '200': '#dfdebf',
-                    '300': '#cecd9f',
-                    '400': '#bebd7f',
-                    '500': '#aeac5f',
-                    '600': '#8b8a4c',
-                    '700': '#686739',
-                    '800': '#464526',
-                    '900': '#232213'
+                    100: '#efeedf',
+                    200: '#dfdebf',
+                    300: '#cecd9f',
+                    400: '#bebd7f',
+                    500: '#aeac5f',
+                    600: '#8b8a4c',
+                    700: '#686739',
+                    800: '#464526',
+                    900: '#232213'
                 }
             }
         },

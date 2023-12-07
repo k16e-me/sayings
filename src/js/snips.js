1c16117fb2a3ccd0907d3eaca307c3e3dcde4951
kb <kb@k16e.co>
2023-12-07 18:16:07 +0100

    ...

--- src/js/snips.js
+++ src/js/snips.js
@@ -1,11 +1,15 @@
+// Functions
 const slugify = str => {
-    str = str.replace(/^\s+|\s+$/g, '') // trim leading/trailing white space
-    str = str.toLowerCase() // convert string to lowercase
-    str = str.replace(/[^a-z0-9 -]/g, '') // remove any non-alphanumeric characters
-        .replace(/\s+/g, '-') // replace spaces with hyphens
-        .replace(/-+/g, '-') // remove consecutive hyphens
+    // Thanks to https://dev.to/bybydev/how-to-slugify-a-string-in-javascript-4o9n
+    str = str.replace(/^\s+|\s+$/g, '')
+    str = str.toLowerCase()
+    str = str
+        .replace(/[^a-z0-9 -]/g, '')
+        .replace(/\s+/g, '-')
+        .replace(/-+/g, '-')
     return str
 }
 
 
+// Exports
 export { slugify }

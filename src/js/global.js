d6d42ec0ae00177c650fba7e782dccfbb86b6823
kb <kb@k16e.co>
2023-12-17 22:28:18 +0100

    flashing when scrolled to

--- src/js/global.js
+++ src/js/global.js
@@ -2,4 +2,19 @@ import { arrayFrom, scrollToTopOffset } from './snips'
 
 (() => {
     console.log('Thank God for Astro 😍')
+
+    const links = arrayFrom('[data-link]')
+    const headerHeight = document.querySelector('[data-main-header]').getBoundingClientRect().height
+
+    links.forEach(link => {
+        link.addEventListener('click', e => {
+            e.preventDefault()
+            const linkEl = document.querySelector(e.target.hash)
+            const parent = linkEl.parentNode
+            scrollToTopOffset(linkEl, headerHeight)
+
+            parent.classList.add('is-focused')
+            parent.addEventListener('animationend', () => parent.classList.remove('is-focused'), false)
+        })
+    })
 })()

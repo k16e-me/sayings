dfb0df5b068c7f951d0b67d88c43e79b0a964c1c
kb <kb@k16e.co>
2023-12-28 22:43:30 +0100

    ...

--- src/js/search.js
+++ src/js/search.js
@@ -13,7 +13,7 @@ export default function Search() {
     const
         pieces = $$('[data-piece]'),
         input = $('#search'),
-        typeInterval = 300
+        typeInterval = 500
 
     input.addEventListener('keyup', () => {
         clearTimeout(typingTimer)
@@ -34,13 +34,14 @@ export default function Search() {
         })
 
         Flip.from(state, {
-            duration: 0.3,
+            duration: 0.5,
             scale: true,
             ease: 'power1.inOut',
             absoluteOnLeave: true,
             prune: true,
             absolute: true,
             fade: true,
+            simple: true,
             onEnter: elements => gsap.fromTo(elements, { opacity: 0, scale: 0 }, { opacity: 1, scale: 1, duration: 0.3 }),
             onLeave: elements => gsap.to(elements, { opacity: 0, scale: 0, duration: 0.3 })
         })

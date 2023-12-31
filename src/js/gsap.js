19dd2e9bad5f214691aff3ab42e51c21fe3d3cb9
kb <kb@k16e.co>
2023-12-31 18:45:59 +0100

    ...

--- src/js/gsap.js
+++ src/js/gsap.js
@@ -5,33 +5,22 @@ import { $ } from './snips'
 gsap.registerPlugin(ScrollTrigger)
 
 const
-    scaleIn = $('[data-gsap="scale-in"]'),
-    cards = gsap.utils.toArray('[data-gsap="move-in"]')
+    scaleIn = $('[data-gsap="scale-in"]')
 
 if (scaleIn) {
-    gsap.from(scaleIn, {
-        scrollTrigger: {
-            trigger: scaleIn,
-            toggleActions: 'restart pause reverse pause'
-        },
-        y: '100%',
-        ease: 'expoScale(0.3,7,none)',
-        opacity: 0,
-        scale: 2,
-        duration: .4
-    })
-}
+    runScaleIn()
 
-// if (cards.length) {
-//     gsap.from(cards, {
-//         scrollTrigger: {
-//             trigger: cards,
-//             toggleActions: 'play none none none'
-//         },
-//         stagger: { each: 0.3 },
-//         y: 24,
-//         ease: 'expoScale(0.3,7,none)',
-//         opacity: 0,
-//         duration: .4
-//     })
-// }
+    function runScaleIn() {
+        gsap.from(scaleIn, {
+            scrollTrigger: {
+                trigger: scaleIn,
+                toggleActions: 'restart pause reverse pause'
+            },
+            y: '100%',
+            ease: 'expoScale(0.3,7,none)',
+            opacity: 0,
+            scale: 2,
+            duration: .4
+        })
+    }
+}

86c3cbf90722a3a715ed6a8bafb6566e911f9c2d
kb <kb@k16e.co>
2024-01-09 08:36:58 +0100

    ...

--- src/js/gsap.js
+++ src/js/gsap.js
@@ -4,15 +4,23 @@ import { $ } from './snips'
 
 gsap.registerPlugin(ScrollTrigger)
 
-const tl = gsap.timeline({ delay: 0.5 })
-const dropIn = {
-    y: -20,
-    duration: 0.4,
-    autoAlpha: 0,
-    ease: 'back.out',
-    clearProps: 'all',
-    transition: 'none'
-}
+const
+    tl = gsap.timeline({
+        delay: 0.5,
+        duration: 0.5
+    }),
+    dropIn = {
+        y: -24,
+        autoAlpha: 0,
+        ease: 'back.out',
+        clearProps: 'all',
+        transition: 'none'
+    },
+    flyIn = {
+        y: -12,
+        scale: 1.15,
+        opacity: 0
+    }
 
 const Silence = () => {
     if (!$('[data-gsap="silence"]')) return
@@ -25,16 +33,6 @@ const Silence = () => {
         ease: 'expoScale(0.3,7,none)',
         opacity: 0,
         scale: 2,
-        duration: .5
-    })
-}
-
-const FlyIn = () => {
-    if (!$('[data-gsap="fly-in"]')) return
-    gsap.from('[data-gsap="fly-in"]', {
-        y: -8,
-        scale: 1.15,
-        opacity: 0,
         duration: .5
     })
 }
@@ -54,7 +52,9 @@ const MoveIn = () => {
 
 const CreateAnimations = () => {
     if (!$('[data-gsap]')) return
+
     tl.from('[data-gsap="logo"]', dropIn, 0)
+    tl.from('[data-gsap="fly-in"]', flyIn, 0)
 }
 
-export { Silence, FlyIn, MoveIn, CreateAnimations }
+export { Silence, MoveIn, CreateAnimations }

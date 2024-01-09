c18b6fbc0e9a51c85df8ac5999a45a62c96be0fb
kb <kb@k16e.co>
2024-01-09 12:42:19 +0100

    ...

--- src/js/gsap.js
+++ src/js/gsap.js
@@ -5,15 +5,15 @@ import { $ } from './snips'
 gsap.registerPlugin(ScrollTrigger)
 
 const
-    tl = gsap.timeline({
+    tl = gsap.timeline({ // Timeline
         defaults: {
             delay: 0.15,
             duration: 0.5,
             autoAlpha: 0,
             opacity: 0
-        },
-        onComplete: () => $('[data-wrapper="main"]').removeAttribute('style')
+        }
     }),
+    // Signatures
     dropIn = {
         y: -24,
         ease: 'back.out',
@@ -26,27 +26,30 @@ const
     riseIn = {
         y: 12,
         scale: 0.99
+    },
+    // Islands
+    Silence = () => {
+        if (!$('[data-gsap="silence"]')) return
+        gsap.from('[data-gsap="silence"]', {
+            scrollTrigger: {
+                trigger: '[data-gsap="silence"]',
+                toggleActions: 'restart pause reverse pause'
+            },
+            y: '100%',
+            ease: 'expoScale(0.3,7,none)',
+            opacity: 0,
+            scale: 2,
+            duration: .4
+        })
+    },
+    runAnimations = () => {
+        $('[data-gsap="logo"]') && tl.from('[data-gsap="logo"]', dropIn)
+        $('[data-gsap="fly-in"]') && tl.from('[data-gsap="fly-in"]', flyIn)
+        $('[data-gsap="rise-in"]') && tl.from('[data-gsap="rise-in"]', {
+            ...riseIn,
+            onComplete: () => $('[data-wrapper="main"]').removeAttribute('style')
+        })
     }
 
-const Silence = () => {
-    if (!$('[data-gsap="silence"]')) return
-    gsap.from('[data-gsap="silence"]', {
-        scrollTrigger: {
-            trigger: '[data-gsap="silence"]',
-            toggleActions: 'restart pause reverse pause'
-        },
-        y: '100%',
-        ease: 'expoScale(0.3,7,none)',
-        opacity: 0,
-        scale: 2,
-        duration: .5
-    })
-}
-
-const runAnimations = () => {
-    $('[data-gsap="logo"]') && tl.from('[data-gsap="logo"]', dropIn)
-    $('[data-gsap="fly-in"]') && tl.from('[data-gsap="fly-in"]', flyIn)
-    $('[data-gsap="rise-in"]') && tl.from('[data-gsap="rise-in"]', riseIn)
-}
 
 export { Silence, runAnimations }

b626c05c182133ee8bc875cfa46fa3540293eb0f
kb <kb@k16e.co>
2024-06-21 07:21:49 +0100

    ...

--- src/scripts/scroll-top-offset.js
+++ src/scripts/scroll-top-offset.js
@@ -3,7 +3,7 @@ import _isMobile from './is-mobile'
 export default function _scrollTopOffset(target, offset) {
     // Credit here @ https://stackoverflow.com/questions/49820013/javascript-scrollintoview-smooth-scroll-and-offset
     const
-        topOffset = _isMobile() ? 120 : offset,
+        topOffset = _isMobile() ? 240 : offset,
         elPos = Math.round(target.getBoundingClientRect().top),
         offsetPos = (elPos + window.scrollY) - (topOffset + 8)
 
